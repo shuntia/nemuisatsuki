825ed9e7b123c9dd6b4ef41d4897d7b366e73f91
Shuntia <shuntia@shuntia.net>
2025-12-29 23:15:12 +0900

    add basic theming and image

---
+++ src/app/fonts.tsx
@@ -0,0 +1,19 @@
+
+import { Public_Sans } from "next/font/google";
+import localFont from "next/font/local"
+
+export const poor_things = localFont({
+  src: "./fonts/POOR_THINGS.ttf",
+});
+
+export const nonbit = localFont({
+  src: "./fonts/213chan_nonbit-Regular.otf",
+});
+
+export const absent_in_the_rain = localFont({
+  src: "./fonts/AbsentInTheRain.otf",
+});
+
+export const public_sans = Public_Sans({
+  subsets: ["latin"],
+});
