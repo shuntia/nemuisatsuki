968342038f291e641cc7b83b8a5d6419256288a0
Shuntia <shuntia@shuntia.net>
2025-12-31 23:26:26 +0900

    adjust layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Card } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import Image from "next/image";
 import Link from "next/link";
-import { absent_in_the_rain } from "./fonts";
+import { absent_in_the_rain, nonbit } from "./fonts";
 import { Arrow } from "./arrow";
 
 export default function Home() {
@@ -14,19 +14,7 @@ export default function Home() {
         style={absent_in_the_rain.style}
       >
         <Image alt="NEMUISATSUKI" src="/logo.png" className="justify-self-start h-full object-contain object-left" width={4096} height={1255} />
-        <div className="flex justify-evenly justify-self-end items-center">
-          <Link href="https://twitter.com/nyaqj">
-            <Button>公式Twitterへ移動</Button>
-          </Link>
-          <div className="w-10" />
-          <Link href="https://nyaqj.myportfolio.com">
-            <Button>公式ポートフォリオへ移動</Button>
-          </Link>
-          <div className="w-10" />
-          <Link href="https://youtube.com/@nyaqj">
-            <Button>公式Youtubeへ移動</Button>
-          </Link>
-        </div>
+
       </div>
       <div className="p-4">
         <Image
@@ -37,19 +25,32 @@ export default function Home() {
           className="w-screen rounded-2xl"
         />
       </div>
-      <div className="h-20" />
       <main className="pl-8 pr-8">
-        <div className="text-xl font-light flex">
-          <Card className="p-10 flex-1 mr-5">
-            <div className="text-4xl font-light">眠井さつき</div>
+        <div className={`text-6xl w-full text-center p-20 text-blue-400 ${nonbit.className}`}>INTRODUCTION</div>
+        <div className="text-xl font-light md:flex">
+          <Card className="md:p-20 sm:p-5 flex-1 md:mr-5">
+            <div className="md:text-4xl sm:text-xl font-light">眠井さつき(ねむい さつき)</div>
             <div className="text-xl mt-10 font-light">
               キミのことを救済するために電子の墓地からやってきた
               <br />
               ばーちゃる墓守Vsinger 眠井さつきです
             </div>
+            <div className="md:flex justify-evenly mt-10 items-center w-2/3">
+              <Link href="https://twitter.com/nyaqj">
+                <Button>公式Twitterへ移動</Button>
+              </Link>
+              <div className="w-10" />
+              <Link href="https://nyaqj.myportfolio.com">
+                <Button>公式ポートフォリオへ移動</Button>
+              </Link>
+              <div className="w-10" />
+              <Link href="https://youtube.com/@nyaqj">
+                <Button>公式Youtubeへ移動</Button>
+              </Link>
+            </div>
           </Card>
-          <Card className="p-10 flex-1 ml-5">
-            <Image src="/nemui_satsuki.png" alt="眠井さつき" width={625} height={946} className="mx-auto" />
+          <Card className="ml-5">
+            <Image src="/nemui_satsuki.png" alt="眠井さつき" width={625} height={946} className="mx-auto scale-75" />
           </Card>
         </div>
       </main>
